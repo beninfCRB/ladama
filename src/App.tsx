0da9ddbcdd5fb4f35b2e935e8fff5d791b248bd5
beninfCRB <beninf10@gmail.com>
2025-09-20 10:06:47 +0700

    add manifest json pwa

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,29 @@ import { RouterProvider } from "react-router";
 import "./App.css";
 import { router } from "./routes";
 import PageProgress from "./components/custom-ui/PageProgress";
+import { useEffect } from "react";
 
 const queryClient = new QueryClient();
 function App() {
+  useEffect(() => {
+    const handler = (e: any) => {
+      e.preventDefault();
+      console.log("beforeinstallprompt fired");
+      e.prompt(); // munculkan popup install
+
+      e.userChoice.then((choiceResult: any) => {
+        if (choiceResult.outcome === "accepted") {
+          console.log("User accepted the install prompt");
+        } else {
+          console.log("User dismissed the install prompt");
+        }
+      });
+    };
+
+    window.addEventListener("beforeinstallprompt", handler);
+    return () => window.removeEventListener("beforeinstallprompt", handler);
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <PageProgress />
